12d7276bde1291fff1257575014d5e68ba29fa1b
Wendy Jiang <wjiang.student@hsfi.us>
2017-07-11 19:22:49 +0000

    fix the carousel

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // JavaScript File
+$('ul li').click(function(){
+    
+    var clicked_tab = $(this);
+    
+    clicked_tab.addClass('active');
+    
+    clicked_tab.parent().children('li').not(clicked_tab).removeClass('active');
+    
+    var clicked_href = $(this).children('a').attr('href');
+    $('img').hide();
+    $(clicked_href).show();
+
+});
+$("#myCarousel").carousel();
+
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
