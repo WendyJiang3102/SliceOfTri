804bc784197e044a46a17aa160f5739e905f25c8
Wendy Jiang <wjiang.student@hsfi.us>
2017-07-18 19:19:21 +0000

    change the podcast page

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 // JavaScript File
-$('ul li').click(function(){
-$("#myCarousel").carousel();
-
-$(".item").click(function(){
-    $("#myCarousel").carousel(1);
-});
-
-$(".left").click(function(){
-    $("#myCarousel").carousel("prev");
-});
-
-$(document).ready(function(){
+$(document).ready(function() {
   $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname) {
       var $target = $(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
+      $target = $target.length && $target ||
+        $('[name=' + this.hash.slice(1) + ']');
       if ($target.length) {
         var targetOffset = $target.offset().top;
         $('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
+          .animate({
+            scrollTop: targetOffset
+          }, 1000);
+        return false;
       }
     }
+  });
+  $('ul li').click(function() {
+    $("#myCarousel").carousel();
+
+    $(".item").click(function() {
+      $("#myCarousel").carousel(1);
+    });
+
+    $(".left").click(function() {
+      $("#myCarousel").carousel("prev");
+    });
   });
 });
