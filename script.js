06d21686df20a32b302165ffe85a4f0abeeedc51
Wendy Jiang <wjiang.student@hsfi.us>
2017-07-20 19:02:05 +0000

    done

--- script.js
+++ script.js
@@ -16,5 +16,12 @@ $(document).ready(function() {
       }
     }
   });
- });
- 
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 10) {
+      $('#nav').addClass('shrink');
+    }
+    else {
+      $('#nav').removeClass('shrink');
+    }
+  });
+});
